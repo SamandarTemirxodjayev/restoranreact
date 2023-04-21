a2bf36c13031bd543876e8fea2c386f238b17ac2
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-21 12:07:44 +0500

    change 404 page

--- client/src/components/404.jsx
+++ client/src/components/404.jsx
@@ -7,12 +7,11 @@ export default function Error404() {
       <div className="">
         <div className="font-bold text-center">
           <h1 className='text-9xl'>404</h1>
-          <div className="text-3xl py-2">Page Not Found</div>
-          <div className="mx-8 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper convallis euismod vestibulum in pharetra.</div>
+          <div className="text-3xl py-2">Sahifa topilmadi</div>
         </div>
         <div className="text-center text-black pt-6">
           <button className='bg-white px-32 py-4 rounded-3xl'>
-            <Link to='/'>Back to Home</Link>
+            <Link to='/'>Bosh Saxifa</Link>
           </button>
         </div>
       </div>
